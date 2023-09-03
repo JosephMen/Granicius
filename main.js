8d7851141c8a90b13440bb28877dae60c0333fa8
franderms <franderms@gmail.com>
2023-09-02 18:37:03 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+
+const form = document.getElementById("main-form")
+const checkResAdd = document.getElementById("check-resAdd")
+
+const mailAdd = [
+    document.getElementById("mail-add"),
+    document.getElementById("mail-add2"),
+    document.getElementById("mail-city"),
+    document.getElementById("mail-zip")
+]
+
+const ressAdd = [
+    document.getElementById("res-add"),
+    document.getElementById("res-add2"),
+    document.getElementById("res-city"),
+    document.getElementById("res-zip")
+]
+
+ressAdd.forEach((input, index) => {
+    $(input).change(function(){
+        $(mailAdd[index]).val($(this).val())
+    })
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const inputsRequired = document.querySelectorAll('input:required')
+    inputsRequired.forEach(input => {
+        if(input.checkValidity()){
+            input.nextElementSibling.classList.remove('spam-display')
+        }
+    })
+
+    if(!form.checkValidity()){
+        event.stopPropagation()
+        form.classList.add("submitted-form")
+
+        const inputsArray = document.querySelectorAll('input:invalid')
+        inputsArray.forEach(input => {
+            input.nextElementSibling.classList.add('spam-display')
+            input.nextElementSibling.textContent = "Field required"
+        })
+
+    } else {
+        console.log('all right')
+    }
+})
+
+checkResAdd.addEventListener('change', event => {
+    if(checkResAdd.checked){
+        mailAdd.forEach((input, index) => {
+            input.disabled = true
+            input.value = ressAdd[index].value
+        })
+        return
+    }
+    mailAdd.forEach((input, index) => {
+        input.disabled = false
+        input.value = ""
+    })
+})
